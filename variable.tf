c32de16484c345320c1184b35bcc1f7a2545c517
surendhar2702 <64633640+surendhar2702@users.noreply.github.com>
2023-11-15 12:26:41 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,10 @@
-
+variable "region" {
+    default = "ap-southeast-1"
+}
+variable "access" {
+    default = "AKIATUBKQA5W4JBDCLUM"
+}
+variable "secret" {
+    default = "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"
+}
+variable "key" {}
